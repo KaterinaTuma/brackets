6dd69a1456fc47369f856971119201d01dda06f5
Katerina Tumakova <1katyakaterina@gmail.com>
2024-09-11 13:35:30 +0300

    feat: implement validBraces

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const state = [];
+  const openingBraces = bracketsConfig.map((pare) => pare[0]);
+  const closingBraces = bracketsConfig.map((pare) => pare[1]);
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charAt(i);
+    const isOpeningBrace = openingBraces.includes(char);
+    const  isClosingBrace = closingBraces.includes(char);
+
+    if (isOpeningBrace && isClosingBrace) {
+      if (state.length > 0 && state[state.length - 1] === char) {
+        state.pop();
+      } else {
+        state.push(char);
+      }
+    } else if (isOpeningBrace) {
+      state.push(char);
+    } else if (isClosingBrace) {
+      const lastOpeningBracket = state.pop();
+      if (!lastOpeningBracket || (openingBraces.indexOf(lastOpeningBracket) !== closingBraces.indexOf(char))) { return false; }
+    }
+  }
+
+  return state.length === 0;
 }
